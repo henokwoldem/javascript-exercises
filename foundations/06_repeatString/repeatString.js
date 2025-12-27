20646c06863eb0eb55f2722769f2b84c8be2f838
henokwoldem <woldemichael@uleth.ca>
2025-12-27 16:28:19 -0700

    Implement function to repeat string x number of times

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let returnStr = "";
+    for(let i = 0; i < num; i++){
+        returnStr += str
+    }
+        
+    return returnStr;
 };
+
+alert(repeatString("hello",3));
 
 // Do not edit below this line
 module.exports = repeatString;
