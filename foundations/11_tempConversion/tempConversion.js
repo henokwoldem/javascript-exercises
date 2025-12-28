7ea08acb27df0092068118f875f9dbb4c5f4ee3e
henokwoldem <woldemichael@uleth.ca>
2025-12-28 15:57:31 -0700

    Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let c =  (5/9) * (fTemp - 32);
+  return +c.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let f =  9/5*cTemp + 32;
+  return +f.toFixed(1);
 };
+
 
 // Do not edit below this line
 module.exports = {
