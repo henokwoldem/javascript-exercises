eef01f3795bb4bdcb20c6628aaecebe868cb2c1f
henokwoldem <woldemichael@uleth.ca>
2025-12-28 14:13:49 -0700

    Implement function o reverse any string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArr = str.split('');
+    let reverseArr = [];
+    let reverseStr = ""
+    for(let i = strArr.length - 1; i >= 0; i--)
+    {
+        reverseArr.push(strArr.pop());
+    }
+    
+    reverseStr = reverseArr.join("");
+    return reverseStr;
 };
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
